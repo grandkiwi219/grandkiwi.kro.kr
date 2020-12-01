16abeb5c27c0cf6e6e29cf4972b7fa34afe9e7e4
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-01 12:07:54 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+## grand kiwi
+
+# Beginner kiwi's github.io
+
+https://grandkiwi219.github.io
---
+++ code/js/koma_UI.js
@@ -0,0 +1,15 @@
+const json
+=
+{
+
+    "header": "<h1 data-aos='zoom-in-up' data-aos-duration='500' style='color:#FEB861;'>코마공화국</h1> <hr data-aos='zoom-in-up' data-aos-duration='500'>",
+    "footer": "<hr> <div>© 코마공화국-grand kiwi  |  <a href='https://github.com/grandkiwi219'>github</a>  |  <a href='https://discord.gg/qtkoma'>discord</a>  |  <a href='/'>power</a></div>",
+    "Koma_2020": "<h1 data-aos='zoom-in-up' data-aos-duration='500' style='color:#FEB861;'>코마공화국 2020</h1> <hr data-aos='zoom-in-up' data-aos-duration='500'>"
+
+}
+
+document.getElementsByTagName("header")[0].innerHTML = json.header;
+
+document.getElementsByTagName("footer")[0].innerHTML = json.footer;
+
+document.getElementById("2020").innerHTML = json.Koma_2020;
