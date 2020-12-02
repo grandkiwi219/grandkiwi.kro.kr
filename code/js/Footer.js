21a7953bf6be1cc5f89a0770cb9c742a6889cc03
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-02 22:58:48 +0900

    Create Footer.js

---
+++ code/js/Footer.js
@@ -0,0 +1,9 @@
+const json
+=
+{
+
+    "footer": "<hr> © grand kiwi's party  |  <a href='https://github.com/grandkiwi219'>github</a>  |  <a href='https://discord.gg/QKbQCWD'>discord</a>  |  <a href='https://github.com/grandkiwi219/grandkiwi219.github.io'>power</a>"
+
+}
+
+document.getElementsByTagName("footer")[0].innerHTML = '<footer>' + json.footer + '</footer>';
