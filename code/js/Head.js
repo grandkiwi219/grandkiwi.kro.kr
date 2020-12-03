3581f66c466c8b1bfd5081b494c922101a2b26d1
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-03 14:00:48 +0900

    Update Head.js

--- code/js/Head.js
+++ code/js/Head.js
@@ -1,4 +1,4 @@
-document.write("<meta charset="utf-8">
+document.write('<meta charset="utf-8">
       <title>grand kiwi's website</title>
       <meta name="viewport", content="width=device-width, initial-scale=1.0, maximum-scale=2.0, minimum-scale=1.0, user-scalable=no">
       <script src="./code/js/Head.js"></script>
@@ -10,4 +10,4 @@ document.write("<meta charset="utf-8">
       <meta property="og:type" content="website">
       <meta property="og:site_name" content="grand kiwi">
       <meta property="theme-color" data-react-helmet="true" content="#96FFCF">
-");
+');
