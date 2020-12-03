3729c26e3f230d6ff3177bdcd980d446923f48b1
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-03 10:02:25 +0900

    Update SideBar.js

--- code/js/SideBar.js
+++ code/js/SideBar.js
@@ -1,4 +1,4 @@
-const json
+const sb
 =
 {
 
@@ -6,4 +6,4 @@ const json
 
 }
 
-document.write('<input type="checkbox" id="navcheck" role="button" title="menu"><label for="navcheck" aria-hidden="true" title="menu"><span class="burger"><span class="bar"><span class="visuallyhidden">Menu</span></span></span></label><nav id="menu">' + json.sidebar + '</nav>');
+document.write('<input type="checkbox" id="navcheck" role="button" title="menu"><label for="navcheck" aria-hidden="true" title="menu"><span class="burger"><span class="bar"><span class="visuallyhidden">Menu</span></span></span></label><nav id="menu">' + sb.sidebar + '</nav>');
