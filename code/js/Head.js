043f960ef6b0e2ee15ef0f88400399fe7fb3ddd0
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-03 14:10:51 +0900

    Update Head.js

--- code/js/Head.js
+++ code/js/Head.js
@@ -1,1 +1,17 @@
-document.write('<meta charset="utf-8"><title>grand kiwi's website</title><meta name="viewport", content="width=device-width, initial-scale=1.0, maximum-scale=2.0, minimum-scale=1.0, user-scalable=no"></script><link rel="shortcut icon" type="image⁄x-icon" href="./img/ticon.png"><link rel="stylesheet" href="./code/css/style.css"><link rel="stylesheet" href="https://unpkg.com/aos@2.3.1/dist/aos.css"><meta property="og:type" content="website"><meta property="og:site_name" content="grand kiwi"><meta property="theme-color" data-react-helmet="true" content="#96FFCF">');
+document.write("
+
+      <meta charset='utf-8'>
+      <title>grand kiwi's website</title>
+      <meta name='viewport', content='width=device-width, initial-scale=1.0, maximum-scale=2.0, minimum-scale=1.0, user-scalable=no'>
+      <script src='./code/js/Head.js'></script>
+
+      <link rel='shortcut icon' type='image⁄x-icon' href='./img/ticon.png'>
+      <link rel='stylesheet' href='./code/css/style.css'>
+      <link rel='stylesheet' href='https://unpkg.com/aos@2.3.1/dist/aos.css'>
+
+      <meta property='og:type' content='website'>
+      <meta property='og:site_name' content='grand kiwi'>
+      <meta property='og:description' content='키위는 안셔요'>
+      <meta property='theme-color' data-react-helmet='true' content='#96FFCF'>
+
+");
