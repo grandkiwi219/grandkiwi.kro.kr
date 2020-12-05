ce46b6c834f7b838c7570508aba4bf8a5484e8b4
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-05 20:59:42 +0900

    Update Footer.js

--- code/js/Footer.js
+++ code/js/Footer.js
@@ -2,7 +2,7 @@ const footer
 =
 {
 
-    "footer": "<hr> © grand kiwi's party  |  <a href='https://github.com/grandkiwi219'>github</a>  |  <a href='https://discord.gg/QKbQCWD'>discord</a>  |  <a href='https://github.com/grandkiwi219/grandkiwi219.github.io'>power</a> <br> <img src='../../img/logo.png' style='width:30px; height:30px;'>"
+    "footer": "<hr> <img src='../../img/logo.png' style='width:40px; height:40px;'> <br> © grand kiwi's party  |  <a href='https://github.com/grandkiwi219'>github</a>  |  <a href='https://discord.gg/QKbQCWD'>discord</a>  |  <a href='https://github.com/grandkiwi219/grandkiwi219.github.io'>power</a>"
 
 }
 
