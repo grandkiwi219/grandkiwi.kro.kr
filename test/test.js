7ebf9f8be0b061e946b5aba3ca6f1dd98219d69b
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-09 22:15:56 +0900

    Create test.js

---
+++ test/test.js
@@ -0,0 +1,15 @@
+
+      var request = new XMLHttpRequest()
+      request.open('GET', 'https://api.namemc.com/profile/04932fd4-d4e6-4aed-80a9-da4a0d64f93e/friends', true)
+      request.onload = function () {
+
+         var data = JSON.parse(this.response)
+
+         data.forEach((friend) => {
+
+           document.write(friend.name)
+           
+         })
+      }
+
+      request.send()
