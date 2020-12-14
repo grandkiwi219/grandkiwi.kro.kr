2b5374795da599f31876188a2b02cefd19236f6c
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-15 08:52:24 +0900

    Update status.js

--- status/status.js
+++ status/status.js
@@ -1,16 +1,22 @@
+const color = {
+    "green": "<img style='width:10px; height:10px;' src='../img/status/green.png'>",
+    "yellow": "<img style='width:10px; height:10px;' src='../img/status/yellow.png'>",
+    "red": "<img style='width:10px; height:10px;' src='../img/status/red.png'>"
+}
+
 var minecraft = new XMLHttpRequest();
 minecraft.open('GET', 'https://status.mojang.com/check', true);
 minecraft.onload = function () {
     var session = JSON.parse(this.response);
 
-    document.getElementById('minecraft').innerHTML += '<p>minecraft.net&emsp;|&emsp;' + session[0]["minecraft.net"] + '</p>';
-    document.getElementById('minecraft').innerHTML += '<p>session.minecraft.net&emsp;|&emsp;' + session[1]["session.minecraft.net"] + '</p>';
-    document.getElementById('minecraft').innerHTML += '<p>account.mojang.com&emsp;|&emsp;' + session[2]["account.mojang.com"] + '</p>';
-    document.getElementById('minecraft').innerHTML += '<p>authserver.mojang.com&emsp;|&emsp;' + session[3]["authserver.mojang.com"] + '</p>';
-    document.getElementById('minecraft').innerHTML += '<p>sessionserver.mojang.com&emsp;|&emsp;' + session[4]["sessionserver.mojang.com"] + '</p>';
-    document.getElementById('minecraft').innerHTML += '<p>api.mojang.com&emsp;|&emsp;' + session[5]["api.mojang.com"] + '</p>';
-    document.getElementById('minecraft').innerHTML += '<p>textures.minecraft.net&emsp;|&emsp;' + session[6]["textures.minecraft.net"] + '</p>';
-    document.getElementById('minecraft').innerHTML += '<p>mojang.com&emsp;|&emsp;' + session[7]["mojang.com"] + '</p>';
+    document.getElementById('minecraft').innerHTML += '<p>minecraft.net&emsp;|&emsp;' + color[session[0]["minecraft.net"]] + '</p>';
+    document.getElementById('minecraft').innerHTML += '<p>session.minecraft.net&emsp;|&emsp;' + color[session[1]["session.minecraft.net"]] + '</p>';
+    document.getElementById('minecraft').innerHTML += '<p>account.mojang.com&emsp;|&emsp;' + color[session[2]["account.mojang.com"]] + '</p>';
+    document.getElementById('minecraft').innerHTML += '<p>authserver.mojang.com&emsp;|&emsp;' + color[session[3]["authserver.mojang.com"]] + '</p>';
+    document.getElementById('minecraft').innerHTML += '<p>sessionserver.mojang.com&emsp;|&emsp;' + color[session[4]["sessionserver.mojang.com"]] + '</p>';
+    document.getElementById('minecraft').innerHTML += '<p>api.mojang.com&emsp;|&emsp;' + color[session[5]["api.mojang.com"]] + '</p>';
+    document.getElementById('minecraft').innerHTML += '<p>textures.minecraft.net&emsp;|&emsp;' + color[session[6]["textures.minecraft.net"]] + '</p>';
+    document.getElementById('minecraft').innerHTML += '<p>mojang.com&emsp;|&emsp;' + color[session[7]["mojang.com"]] + '</p>';
 
       }
 minecraft.send();
