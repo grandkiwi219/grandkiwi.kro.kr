31e707f44192a50f059bc9eb45265fb18d178d91
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-15 01:04:14 +0900

    Update status.js

--- status/status.js
+++ status/status.js
@@ -3,16 +3,14 @@ minecraft.open('GET', 'https://status.mojang.com/check', true);
 minecraft.onload = function () {
     var session = JSON.parse(this.response);
 
-    var doc_mc = document.getElementById('minecraft').innerHTML;
-
-    doc_mc += '<p>minecraft.net&emsp;|&emsp;' + session[0]["minecraft.net"] + '</p>';
-    doc_mc += '<p>session.minecraft.net&emsp;|&emsp;' + session[1]["session.minecraft.net"] + '</p>';
-    doc_mc += '<p>account.mojang.com&emsp;|&emsp;' + session[2]["account.mojang.com"] + '</p>';
-    doc_mc += '<p>authserver.mojang.com&emsp;|&emsp;' + session[3]["authserver.mojang.com"] + '</p>';
-    doc_mc += '<p>sessionserver.mojang.com&emsp;|&emsp;' + session[4]["sessionserver.mojang.com"] + '</p>';
-    doc_mc += '<p>api.mojang.com&emsp;|&emsp;' + session[5]["api.mojang.com"] + '</p>';
-    doc_mc += '<p>textures.minecraft.net&emsp;|&emsp;' + session[6]["textures.minecraft.net"] + '</p>';
-    doc_mc += '<p>mojang.com&emsp;|&emsp;' + session[7]["mojang.com"] + '</p>';
+    document.getElementById('minecraft').innerHTML += '<p>minecraft.net&emsp;|&emsp;' + session[0]["minecraft.net"] + '</p>';
+    document.getElementById('minecraft').innerHTML += '<p>session.minecraft.net&emsp;|&emsp;' + session[1]["session.minecraft.net"] + '</p>';
+    document.getElementById('minecraft').innerHTML += '<p>account.mojang.com&emsp;|&emsp;' + session[2]["account.mojang.com"] + '</p>';
+    document.getElementById('minecraft').innerHTML += '<p>authserver.mojang.com&emsp;|&emsp;' + session[3]["authserver.mojang.com"] + '</p>';
+    document.getElementById('minecraft').innerHTML += '<p>sessionserver.mojang.com&emsp;|&emsp;' + session[4]["sessionserver.mojang.com"] + '</p>';
+    document.getElementById('minecraft').innerHTML += '<p>api.mojang.com&emsp;|&emsp;' + session[5]["api.mojang.com"] + '</p>';
+    document.getElementById('minecraft').innerHTML += '<p>textures.minecraft.net&emsp;|&emsp;' + session[6]["textures.minecraft.net"] + '</p>';
+    document.getElementById('minecraft').innerHTML += '<p>mojang.com&emsp;|&emsp;' + session[7]["mojang.com"] + '</p>';
 
       }
 minecraft.send();
