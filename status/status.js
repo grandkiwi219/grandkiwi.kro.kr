35c4b7d99299b59825642fedea5eb2788507078c
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-14 23:54:29 +0900

    Update status.js

--- status/status.js
+++ status/status.js
@@ -6,13 +6,13 @@ minecraft.onload = function () {
     const doc_mc = document.getElementById('minecraft').innerHTML
 
     doc_mc += '<p>minecraft.net&emsp;|&emsp;' + session["minecraft.net"] + '</p>';
-    doc_mc += '<p>session.minecraft.net&emsp;|&emsp;' + session[session.minecraft.net] + '</p>';
-    doc_mc += '<p>account.mojang.com&emsp;|&emsp;' + session[account.mojang.com] + '</p>';
-    doc_mc += '<p>authserver.mojang.com&emsp;|&emsp;' + session[authserver.mojang.com] + '</p>';
-    doc_mc += '<p>sessionserver.mojang.com&emsp;|&emsp;' + session[sessionserver.mojang.com] + '</p>';
-    doc_mc += '<p>api.mojang.com&emsp;|&emsp;' + session[api.mojang.com] + '</p>';
-    doc_mc += '<p>textures.minecraft.net&emsp;|&emsp;' + session[textures.minecraft.net] + '</p>';
-    doc_mc += '<p>mojang.com&emsp;|&emsp;' + session[mojang.com] + '</p>';
+    doc_mc += '<p>session.minecraft.net&emsp;|&emsp;' + session["session.minecraft.net"] + '</p>';
+    doc_mc += '<p>account.mojang.com&emsp;|&emsp;' + session["account.mojang.com"] + '</p>';
+    doc_mc += '<p>authserver.mojang.com&emsp;|&emsp;' + session["authserver.mojang.com"] + '</p>';
+    doc_mc += '<p>sessionserver.mojang.com&emsp;|&emsp;' + session["sessionserver.mojang.com"] + '</p>';
+    doc_mc += '<p>api.mojang.com&emsp;|&emsp;' + session["api.mojang.com"] + '</p>';
+    doc_mc += '<p>textures.minecraft.net&emsp;|&emsp;' + session["textures.minecraft.net"] + '</p>';
+    doc_mc += '<p>mojang.com&emsp;|&emsp;' + session["mojang.com"] + '</p>';
 
       }
 minecraft.send();
