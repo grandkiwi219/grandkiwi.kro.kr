ca4b1d7dfd366a0397f6345c1720be4048e92631
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-15 23:15:32 +0900

    Create hypixel.js

---
+++ status/hypixel.js
@@ -0,0 +1,14 @@
+var hp = new StatusPage.page({ page : 'qhddg0t7xyb0' });
+hp.summary({
+  success : function(data) {
+         document.getElementById('hypixel').innerHTML += '<hr>' + data.status.description;
+
+     data.components.forEach(slice => {
+         document.getElementById('hypixel').innerHTML += '<hr>';
+         document.getElementById('hypixel').innerHTML += '<div>';
+         document.getElementById('hypixel').innerHTML += slice.name + '&emsp;|&emsp;' + dis[slice.status];
+         if (slice.description !== null) { document.getElementById('hypixel').innerHTML += '<br><span style="color:silver;">DESCRIPTION</span><br>' + slice.description; }
+         document.getElementById('hypixel').innerHTML += '</div>';
+      })
+   }
+});
