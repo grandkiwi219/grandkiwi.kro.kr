2391acc38c47ddf0aac7a9ba7d79230ab857edb1
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-15 20:26:34 +0900

    Update test.js

--- test/test.js
+++ test/test.js
@@ -7,7 +7,9 @@
 
          data.forEach((friend) => {
 
-           document.getElementById('test').innerHTML += '<p>' + friend.name + '</p>';
+           document.getElementById('test').innerHTML += '<p id="testing">' + friend.name + '</p>';
+
+           setTimeout(() => document.getElementById('testing').innerHTML += '반갑다 인간', 5000)
            
          })
       }
