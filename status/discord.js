3c532eb7a8c1b3b62f9629e6d01c7765d2ba13fa
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-16 11:44:55 +0900

    Update discord.js

--- status/discord.js
+++ status/discord.js
@@ -17,49 +17,38 @@ sp.summary({
    success: function(data) {
       document.getElementById('discord').innerHTML += '<hr>' + sys[data.status.description] + '<hr>';
 
-      document.getElementById('discord').innerHTML += '<div>' + data.components[1].name + '&emsp;|&emsp;' + dis[data.components[1].status];
+      document.getElementById('discord').innerHTML += data.components[1].name + '&emsp;|&emsp;' + dis[data.components[1].status];
       if (data.components[1].description !== null) { document.getElementById('discord').innerHTML += '<br><span style="color:silver;">DESCRIPTION</span><br>' + data.components[1].description; }
-      document.getElementById('discord').innerHTML += '</div>';
 
-      document.getElementById('discord').innerHTML += '<hr><div>' + data.components[3].name + '&emsp;|&emsp;' + dis[data.components[3].status];
+      document.getElementById('discord').innerHTML += '<hr>' + data.components[3].name + '&emsp;|&emsp;' + dis[data.components[3].status];
       if (data.components[3].description !== null) { document.getElementById('discord').innerHTML += '<br><span style="color:silver;">DESCRIPTION</span><br>' + data.components[3].description; }
-      document.getElementById('discord').innerHTML += '</div>';
 
-      document.getElementById('discord').innerHTML += '<hr><div>' + data.components[6].name + '&emsp;|&emsp;' + dis[data.components[6].status];
+      document.getElementById('discord').innerHTML += '<hr>' + data.components[6].name + '&emsp;|&emsp;' + dis[data.components[6].status];
       if (data.components[6].description !== null) { document.getElementById('discord').innerHTML += '<br><span style="color:silver;">DESCRIPTION</span><br>' + data.components[6].description; }
-      document.getElementById('discord').innerHTML += '</div>';
 
-      document.getElementById('discord').innerHTML += '<hr><div>' + data.components[9].name + '&emsp;|&emsp;' + dis[data.components[9].status];
+      document.getElementById('discord').innerHTML += '<hr>' + data.components[9].name + '&emsp;|&emsp;' + dis[data.components[9].status];
       if (data.components[9].description !== null) { document.getElementById('discord').innerHTML += '<br><span style="color:silver;">DESCRIPTION</span><br>' + data.components[9].description; }
-      document.getElementById('discord').innerHTML += '</div>';
 
-      document.getElementById('discord').innerHTML += '<hr><div>' + data.components[11].name + '&emsp;|&emsp;' + dis[data.components[11].status];
+      document.getElementById('discord').innerHTML += '<hr>' + data.components[11].name + '&emsp;|&emsp;' + dis[data.components[11].status];
       if (data.components[11].description !== null) { document.getElementById('discord').innerHTML += '<br><span style="color:silver;">DESCRIPTION</span><br>' + data.components[11].description; }
-      document.getElementById('discord').innerHTML += '</div>';
-
 
    data.components.forEach(slice => {
 
        if (slice.name == 'API' || slice.name == 'CloudFlare' || slice.name == 'Media Proxy' || slice.name == 'Tax Calculation Service' || slice.name == 'Push Notifications' || slice.name == 'Search' || slice.name == 'Voice' || slice.name == 'Third-party') return;
 
        document.getElementById('discord').innerHTML += '<hr>';
-       document.getElementById('discord').innerHTML += '<div>';
        document.getElementById('discord').innerHTML += slice.name + '&emsp;|&emsp;' + dis[slice.status];
        if (slice.description !== null) { document.getElementById('discord').innerHTML += '<br><span style="color:silver;">DESCRIPTION</span><br>' + slice.description; }
-       document.getElementById('discord').innerHTML += '</div>';
     })
 
-      document.getElementById('discord').innerHTML += '<hr><div>' + data.components[12].name + '&emsp;|&emsp;' + dis[data.components[12].status];
+      document.getElementById('discord').innerHTML += '<hr>' + data.components[12].name + '&emsp;|&emsp;' + dis[data.components[12].status];
       if (data.components[12].description !== null) { document.getElementById('discord').innerHTML += '<br><span style="color:silver;">DESCRIPTION</span><br>' + data.components[12].description; }
-      document.getElementById('discord').innerHTML += '</div>';
 
-      document.getElementById('discord').innerHTML += '<hr><div>' + data.components[2].name + '&emsp;|&emsp;' + dis[data.components[2].status];
+      document.getElementById('discord').innerHTML += '<hr>' + data.components[2].name + '&emsp;|&emsp;' + dis[data.components[2].status];
       if (data.components[2].description !== null) { document.getElementById('discord').innerHTML += '<br><span style="color:silver;">DESCRIPTION</span><br>' + data.components[2].description; }
-      document.getElementById('discord').innerHTML += '</div>';
 
-      document.getElementById('discord').innerHTML += '<hr><div>' + data.components[4].name + '&emsp;|&emsp;' + dis[data.components[4].status];
+      document.getElementById('discord').innerHTML += '<hr>' + data.components[4].name + '&emsp;|&emsp;' + dis[data.components[4].status];
       if (data.components[4].description !== null) { document.getElementById('discord').innerHTML += '<br><span style="color:silver;">DESCRIPTION</span><br>' + data.components[4].description; }
-      document.getElementById('discord').innerHTML += '</div>';
 
   }
 })
