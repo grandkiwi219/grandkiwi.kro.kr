b8a52d4f3ff69107fe315daf733aa07e1d07ea6f
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-16 10:56:19 +0900

    Update cloudflare.js

--- status/cloudflare.js
+++ status/cloudflare.js
@@ -3,7 +3,7 @@ cf.summary({
   success : function(data) {
          document.getElementById('cloudflare').innerHTML += '<hr>' + sys[data.status.description];
     
-         document.getElementById('cloudflare').innerHTML += '<hr><a href="javascript:void(0)" id="a_show" onclick="click_event();">[ 펼치기 ]</a><div id="div_hide" style="display:none;">';
+         document.getElementById('cloudflare').innerHTML += '<hr><a href="#cloudflare" id="a_show" onclick="click_event();">[ 펼치기 ]</a><div id="div_hide" style="display:none;">';
      
          function click_event () {
            if (div_hide.style.display == "none") { div_hide.style.display=""; a_show.innerText = "[ 접기 ]" } else { div_hide.style.display="none"; a_show.innerText = "[ 펼치기 ]" }
