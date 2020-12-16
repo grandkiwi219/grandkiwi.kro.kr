f0f377458a290f23d5190515c0e592c952830714
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-16 22:08:55 +0900

    Update function.js

--- code/js/function.js
+++ code/js/function.js
@@ -3,11 +3,11 @@ function folding (me, other) {
     var otherElement = document.getElementById(other)
 
     if (meElement.innerText == "[ 펼치기 ]") {
+        otherElement.classList.add('open');
         meElement.innerText = "[ 접기 ]"
     } else { 
+        otherElement.classList.remove('open');
         meElement.innerText = "[ 펼치기 ]"
     }
-
-    otherElement.classList.toggle('open');
 
 }
