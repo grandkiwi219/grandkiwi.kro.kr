ed7beb2e3de5fcb8797382dcc81caefbc3cbc4f0
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-16 11:43:15 +0900

    Update github.js

--- status/github.js
+++ status/github.js
@@ -5,10 +5,8 @@ gh.summary({
 
      data.components.forEach(slice => {
          document.getElementById('github').innerHTML += '<hr>';
-         document.getElementById('github').innerHTML += '<div>';
          document.getElementById('github').innerHTML += slice.name + '&emsp;|&emsp;' + dis[slice.status];
          if (slice.description !== null) { document.getElementById('github').innerHTML += '<br><span style="color:silver;">DESCRIPTION</span><br>' + slice.description; }
-         document.getElementById('github').innerHTML += '</div>';
       })
    }
 });
