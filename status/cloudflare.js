90cf2470b0cdd4641c3ca812a77903f35c0d5a85
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-16 11:42:34 +0900

    Update cloudflare.js

--- status/cloudflare.js
+++ status/cloudflare.js
@@ -3,22 +3,19 @@ cf.summary({
   success : function(data) {
          document.getElementById('cloudflare').innerHTML += '<hr>' + sys[data.status.description];
     
-         document.getElementById('cloudflare').innerHTML += '<hr><a href="#cloudflare" id="a_show" onclick="click_event();">[ 펼치기 ]</a><div id="div_hide" style="visibility:hidden;">';
+         document.getElementById('cloudflare').innerHTML += '<hr><a href="#cloudflare" id="a_show" onclick="click_event();">[ 펼치기 ]</a><div id="div_hide" style="display:none;">';
      
 
      data.components.forEach(slice => {
-         document.getElementById('cloudflare').innerHTML += '<hr>';
-         document.getElementById('cloudflare').innerHTML += '<div>';
-         document.getElementById('cloudflare').innerHTML += slice.name + '&emsp;|&emsp;' + dis[slice.status];
-         if (slice.description !== null) { document.getElementById('cloudflare').innerHTML += '<br><span style="color:silver;">DESCRIPTION</span><br>' + slice.description; }
-         document.getElementById('cloudflare').innerHTML += '</div>';
+         document.getElementById('div_hidden').innerHTML += '<hr>';
+         document.getElementById('div_hidden').innerHTML += slice.name + '&emsp;|&emsp;' + dis[slice.status];
+         if (slice.description !== null) { document.getElementById('div_hidden').innerHTML += '<br><span style="color:silver;">DESCRIPTION</span><br>' + slice.description; }
       })
-    
-        document.getElementById('cloudflare').innerHTML += '</div>';
+  
     
    }
 });
 
 function click_event () {
-  if (div_hide.style.visibility == "hidden") { div_hide.style.visibility=""; a_show.innerText = "[ 접기 ]" } else { div_hide.style.visibility="hidden"; a_show.innerText = "[ 펼치기 ]" }
+  if (div_hide.style.display == "none") { div_hide.style.display=""; a_show.innerText = "[ 접기 ]" } else { div_hide.style.display="none"; a_show.innerText = "[ 펼치기 ]" }
 }
