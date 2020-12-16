293c0a210e3826ecdb78a9619af35fbfa18a6f9f
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-16 10:27:56 +0900

    Update cloudflare.js

--- status/cloudflare.js
+++ status/cloudflare.js
@@ -2,6 +2,8 @@ var cf = new StatusPage.page({ page : 'yh6f0r4529hb' });
 cf.summary({
   success : function(data) {
          document.getElementById('cloudflare').innerHTML += '<hr>' + sys[data.status.description];
+    
+         document.getElementById('cloudflare').innerHTML += '<a href="javascript:void(0)" id="a_show" onclick="if (div_hide.style.display == "none") { div_hide.style.display=''; show.innerText = "[ 접기 ]" } else { div_hide.style.innerText = "[ 펼치기 ]" }">[ 펼치기 ]</a><div id=""div_hide" style="display:none;">';
 
      data.components.forEach(slice => {
          document.getElementById('cloudflare').innerHTML += '<hr>';
@@ -10,5 +12,8 @@ cf.summary({
          if (slice.description !== null) { document.getElementById('cloudflare').innerHTML += '<br><span style="color:silver;">DESCRIPTION</span><br>' + slice.description; }
          document.getElementById('cloudflare').innerHTML += '</div>';
       })
+    
+        document.getElementById('cloudflare').innerHTML += '</div>';
+    
    }
 });
