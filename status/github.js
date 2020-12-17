7875860d458ea7f3ec0cd98c17cfff8d66396412
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-17 11:01:03 +0900

    Update github.js

--- status/github.js
+++ status/github.js
@@ -1,6 +1,8 @@
 setTimeout(() => {
     if (document.getElementById('load-github').innerHTML == '<img style="width:18px; height:18px;" src="../img/status/loading.gif">&emsp;|&emsp;로딩 중..') {
         document.getElementById('load-github').innerHTML = errorTime;
+    } else {
+        console.log('github api loading success')
     }
 }, 10000) //10s
 
