1130512ef3ad1f28658982e06e42651e509d7eec
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-18 09:34:04 +0900

    Update system.js

--- code/js/system.js
+++ code/js/system.js
@@ -1,3 +1,5 @@
+// Internet Explorer notice
+
 var agent = navigator.userAgent.toLowerCase();
 
 if ( (navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1) ) {
