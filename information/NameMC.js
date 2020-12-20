6e5a9794bba7177117af7e2db4016864eebbc4c6
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-20 18:02:32 +0900

    Update NameMC.js

--- information/NameMC.js
+++ information/NameMC.js
@@ -4,13 +4,13 @@ request.onload = function () {
 
     var data = JSON.parse(this.response);
 
-    document.getElementById('name-mc').innerHTML += '<div class="point" data-aos="fade-up" data-aos-duration="1000"><a href="https://mine.ly/grand_kiwi219.1">grand kiwi NameMC</a></div><br>';
+    document.getElementById('name-mc').innerHTML += '<div class="point" data-aos="fade-up" data-aos-duration="1000"><a href="https://namemc.com/profile/grand_kiwi219.1">grand kiwi NameMC</a></div><br>';
 
     document.getElementById('name-mc').innerHTML += '<div class="point" data-aos="fade-up" data-aos-duration="1000">-<span class="silver"> NameMC 친구 리스트 </span>-</div><br>';
 
     data.forEach((friend) => {
 
-        document.getElementById('name-mc').innerHTML += '<div class="point" data-aos="fade-up" data-aos-duration="1000"><a href="https://mine.ly/'+ friend.name + '">' + friend.name + '</a></div>';
+        document.getElementById('name-mc').innerHTML += '<div class="point" data-aos="fade-up" data-aos-duration="1000"><a href="https://namemc.com/profile/'+ friend.name + '">' + friend.name + '</a></div>';
 
     })
 
