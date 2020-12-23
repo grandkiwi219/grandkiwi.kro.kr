348fb17d273405d23d77f55148efceac13f1141f
grand_kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-23 20:47:07 +0900

    Update SideBar.js

--- code/js/SideBar.js
+++ code/js/SideBar.js
@@ -5,7 +5,7 @@ const sb
 '<a href="/information">Information</a>',
 '<a href="/community">Community</a>',
 '<a href="/project">Project</a>',
-'<sub style="color:red;">new</sub><a href="/status">Status</a>',
+'<a href="/status"><sub style="color:#ff7385;">new</sub>Status</a>',
 '',
 '',
 '',
