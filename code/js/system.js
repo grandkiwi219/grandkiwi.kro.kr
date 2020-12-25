87f4abca780c5cd5978095ef29a5c3eb921372fa
grand kiwi <50535288+grandkiwi219@users.noreply.github.com>
2020-12-26 01:12:22 +0900

    Update system.js

--- code/js/system.js
+++ code/js/system.js
@@ -14,6 +14,8 @@ if ( (navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') !
 
 // notice
 
+//var notice = 'Happy New Year! ✨<br/>해피 유희열!'
+
 var warn_notice = '<br/><span style="color:silver;">주의</span> ※ <span style="color:silver;">IE에서는 로딩이 안될 수 있습니다</span>'
 
 
