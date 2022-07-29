69a5739417a4bef3ca5a7d248804dd963d7ced4f
grand kiwi <50535288+grandkiwi219@users.noreply.github.com>
2022-07-30 02:00:43 +0900

    Update SideBar.js

--- code/js/SideBar.js
+++ code/js/SideBar.js
@@ -11,6 +11,7 @@ const sb
 '',
 '',
 '',
+''
 ]
 
 document.write('<input type="checkbox" id="navcheck" role="button" title="menu"><label for="navcheck" aria-hidden="true" title="menu"><span class="burger"><span class="bar"><span class="visuallyhidden">Menu</span></span></span></label><nav id="menu">' + sb[0] + sb[1] + sb[2] + sb[3] + sb[4] + sb[5] + sb[6] + sb[7] + sb[8] + sb[9] + '</nav>');
