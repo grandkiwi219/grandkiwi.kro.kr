e2aff2bfbb2a9d2d5bb252d2aab3c3a8d20dbd3a
grand kiwi <50535288+grandkiwi219@users.noreply.github.com>
2022-08-31 21:36:00 +0900

    Update koma_UI.js

--- code/js/other/koma_UI.js
+++ code/js/other/koma_UI.js
@@ -3,7 +3,7 @@ const json
 {
 
     "header": "<h1 data-aos='zoom-in-up' data-aos-duration='500' style='color:#FEB861;'>코마공화국</h1> <hr data-aos='zoom-in-up' data-aos-duration='500'>",
-    "footer": "<hr> <div>© 코마공화국-grand kiwi  |  <a href='https://github.com/grandkiwi219'>github</a>  |  <a href='https://discord.gg/qtkoma'>discord</a>  |  <a href='/'>power</a></div>",
+    "footer": "<hr> <div>© 코마공화국-grand kiwi  |  <a href='https://github.com/grandkiwi219'>github</a>  |  <a href='https://discord.gg/U2U6E4yUTZ'>discord</a>  |  <a href='/'>power</a></div>",
     "Koma_2020": "<h1 data-aos='zoom-in-up' data-aos-duration='500'><a style='color:#FEB861;' href='/koma'>코마공화국 2020</a></h1> <hr data-aos='zoom-in-up' data-aos-duration='500'>"
 
 }
